43f09edb58e7f5a68196fb79e112eb6cda4be2fc
Eric Wright <ericwright.fastmail.f>
2011-03-21 08:07:07 -0400

    adding FAQ and Terms links to footer

--- footer.php
+++ footer.php
@@ -9,7 +9,17 @@
 			</h2>
 
 			<p>
-				Follow @<a href="http://twitter.com/acedept">acedept</a> on Twitter, find us on <a href="http://www.facebook.com/pages/Ace-Department/173136476045079">Facebook</a>, or <a href="/contact">contact us</a>
+				Follow @<a href="http://twitter.com/acedept">acedept</a> on Twitter or find us on <a href="http://www.facebook.com/pages/Ace-Department/173136476045079">Facebook</a>
+
+				<span class="separator">&bull;</span>
+				<a href="/faq">Frequently Asked Questions</a>
+
+				<span class="separator">&bull;</span>
+				<a href="/terms-of-service">Terms of Service</a>
+
+				<span class="separator">&bull;</span>
+				<a href="/contact">Contact Us</a>
+
 			</p>
 
 			<?php do_action( 'bp_footer' ) ?>
