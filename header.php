2dcdbef1b315fbfd0935c0825ae4d33c3a589477
Eric Wright <ericwright.fastmail.f>
2011-03-23 08:12:33 -0400

    adding google analytics

--- header.php
+++ header.php
@@ -36,7 +36,11 @@
 		<!-- typekit -->
 		<script type="text/javascript" src="http://use.typekit.com/gxc4dpr.js"></script>
 		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
-		
+
+		<!-- Google Analytics -->
+		<script type="text/javascript">
+		var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-4478035-6']); _gaq.push(['_trackPageview']); (function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();
+		</script>		
 
 	</head>
 
