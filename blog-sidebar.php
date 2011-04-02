ed273ca77ecd5540f414e46fb98a561d978b07dc
Eric Wright <ericwright.fastmail.f>
2011-04-02 15:26:42 -0400

    updates to sidebar -- stub content for active members

--- blog-sidebar.php
+++ blog-sidebar.php
@@ -4,6 +4,24 @@
 		<?php include "search-form.php"; ?>
 	</div>
 
+	<div class="sidebar-box contact">
+		<a href="http://twitter.com/acedept" class="twitter">Twitter</a>
+		<a href="http://www.facebook.com/pages/Ace-Department/173136476045079" class="facebook">Facebook</a>
+		<a href="http://www.acedepartment.com/activity/feed/" class="rss">RSS</a>
+		<a href="/contact" class="contact">Contact</a>
+	</div>
+
+	<div class="sidebar-box active-members">
+		<h4>Active Members</h4>
+		plugin content goes here
+	</div>
+
+	<div class="sidebar-box ask-ace">
+		<a href="/contact">Ask Ace</a>
+	</div>
+
+	
+
 	<div class="sidebar-box also-like">
 		<h4>You May Also Like...</h4>
 		<div class="linkwithin_div"></div>
